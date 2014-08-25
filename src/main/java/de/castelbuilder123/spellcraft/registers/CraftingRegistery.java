11b3bc7608afa7d93208c59d22bcdd98c079ca96
Castelbuilder123 <Jacky.21@gmx.de>
2014-08-25 17:25:38 +0200

    Added crafting recipes of the ingame "wikis"

--- src/main/java/de/castelbuilder123/spellcraft/registers/CraftingRegistery.java
+++ src/main/java/de/castelbuilder123/spellcraft/registers/CraftingRegistery.java
@@ -2,6 +2,7 @@ package de.castelbuilder123.spellcraft.registers;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class CraftingRegistery
@@ -17,6 +18,8 @@ public class CraftingRegistery
                     "dnd",
                     "ddd",
                     'd', new ItemStack(Items.diamond), 'n', new ItemStack(Items.nether_star));
+            GameRegistry.addShapelessRecipe(new ItemStack(ItemRegistery.BookOfMagicItem), new ItemStack(ItemRegistery.PowerOfTheBrightItem), new ItemStack(Items.book));
+            GameRegistry.addShapelessRecipe(new ItemStack(ItemRegistery.BookOfDarkMagicItem), new ItemStack(ItemRegistery.PowerOfTheDarknessItem), new ItemStack(Items.book));
 			registered = true;
 		}
 	}
