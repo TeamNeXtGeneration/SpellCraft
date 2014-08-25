f9d23903ad01c12404ef641abde83e66a856347b
Joe Barley Laptop <barley.joseph@gmail.com>
2014-08-25 19:05:06 +0100

    Ore Generator

---
+++ src/main/java/de/castelbuilder123/spellcraft/worldgen/SpellCraftOreGenerator.java
@@ -0,0 +1,15 @@
+package de.castelbuilder123.spellcraft.worldgen;
+
+import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+
+import java.util.Random;
+
+public class SpellCraftOreGenerator implements IWorldGenerator {
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+
+    }
+}
