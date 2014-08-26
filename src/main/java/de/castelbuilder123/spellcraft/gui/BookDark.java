43ea01f43ec6b8c2fba74886a7e85684e67f8ed2
Joe Barley Laptop <barley.joseph@gmail.com>
2014-08-26 15:19:55 +0100

    Books and Gui

--- src/main/java/de/castelbuilder123/spellcraft/gui/BookDark.java
+++ src/main/java/de/castelbuilder123/spellcraft/gui/BookDark.java
@@ -2,6 +2,7 @@ package de.castelbuilder123.spellcraft.gui;
 
 import de.castelbuilder123.spellcraft.network.PacketDecision;
 import de.castelbuilder123.spellcraft.network.PacketHandler;
+import de.castelbuilder123.spellcraft.utils.gui.Grid;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 
@@ -10,8 +11,14 @@ public class BookDark extends GuiScreen {
 
     public static int GUI_ID = 3;
 
-    public BookDark() {
+    public int oldMouseX = this.width/2;
+    public int oldMouseY = this.height/2;
 
+    public Grid grid = new Grid();
+
+    public BookDark() {
+        grid.centerX=0;
+        grid.centerY=0;
     }
 
     public void initGui() {
@@ -21,6 +28,18 @@ public class BookDark extends GuiScreen {
 
     public void updateScreen() {
         super.updateScreen();
+    }
+
+    @Override
+    protected void mouseClickMove(int mouseX, int mouseY, int lastButtonClicked, long timeSinceMouseClick)
+    {
+        int changeX = oldMouseX - mouseX;
+        int changeY = oldMouseY - mouseY;
+        grid.centerX -= changeX;
+        grid.centerY -= changeY;
+        //At the end
+        oldMouseX = mouseX;
+        oldMouseY = mouseY;
     }
 
 
---
+++ src/main/java/de/castelbuilder123/spellcraft/utils/gui/Grid.java
@@ -0,0 +1,9 @@
+package de.castelbuilder123.spellcraft.utils.gui;
+
+public class Grid {
+    public int centerX,centerY;
+    public Grid()
+    {
+
+    }
+}
